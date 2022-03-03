e54e3f0b9401f40ad77f57650dd47d1626dfa9ee
fran zabaleta <franzabaletacordo@gmail.com>
2022-03-03 13:53:24 +0100

    shop modificado

--- Proy/shop.php
+++ Proy/shop.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tienda Panaderia</title>
+    <title>Tienda Panaderiaadasdda</title>
     <link rel="stylesheet" href="view/css/shop.css">
 </head>
 <body>
